af68ca30969845708ae58c21ecf3e5ad14ae83e9
josphatmaron <josphatmaron@gmail.com>
2025-05-27 22:20:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://josphatmaron:wera2025@cluster0.8n4u6dl.mongodb.net/airpawa?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected!'))
+.catch(err => console.error('MongoDB connection error:', err));
+
 const menuToggle = document.getElementById('menu-toggle');
 const dropdownMenu = document.getElementById('dropdown-menu');
 
