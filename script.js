f89ce2366da8942e67d2e72584db33c0a9f74348
josphatmaron <josphatmaron@gmail.com>
2025-06-03 08:54:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -108,8 +108,8 @@ function drawRadiatingLines() {
   const dpr = window.devicePixelRatio || 1;
   const logicalHeight = canvas.height / dpr;
   // Add your offset values here
-  const offsetX = -50; // move left (negative value)
-  const offsetY = 50;  // move down (positive value)
+  const offsetX = 0; // move left (negative value)
+  const offsetY = 0;  // move down (positive value)
   const centerX = 0 + offsetX;
   const centerY = logicalHeight + offsetY;
   const radius = calculateMaxRadius(centerX, centerY);
